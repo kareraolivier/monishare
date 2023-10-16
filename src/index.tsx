6f1109c9a7497a735427488db6f500f11345359f
Matthias Moosburger <matthias.moosburger@gmail.com>
2023-10-16 08:23:03 +0200

    fix: don't lint SVG and CSS; fix husky commitlint

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
