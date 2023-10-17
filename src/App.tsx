5916c70f5cb6ce285f170f00ab881466dd9cb436
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-17 19:14:54 +0200

    feat: adding Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
-import LockIcon from './assets/LockIcon'
-import PlateIcon from './assets/PlateIcon'
 
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
@@ -13,12 +11,10 @@ configure({
 
 function App(): ReactElement {
   return (
-    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10 bg-indigo-600">
+    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
       <h1 className="text-4xl font-bold">Hello to MoniShare</h1>
       <p>If you can read this, you have successfully started the base frontend repository!</p>
       <p>Happy coding!</p>
-      <LockIcon />
-      <PlateIcon />
     </main>
   )
 }
