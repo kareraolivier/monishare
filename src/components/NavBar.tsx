07bc642be29b679afdce3a6dd0b684b7d30b5146
gsngdavid <gsngdavid@gmail.com>
2023-10-18 17:34:48 +0200

    feat: change the menu to close when the menu is opened

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,64 +14,68 @@ function NavBar() {
   return (
     <div className="relative flex items-center justify-between bg-gray-800 p-5">
       <Menu>
-        <Menu.Button className="text-gray-100">Menu</Menu.Button>
-        <Menu.Items className="absolute top-24 flex flex-col rounded-lg border bg-indigo-400 text-gray-100">
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/book">
-                <CarIcon />
-                <span>Book A Car</span>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/bookings">
-                <PlateIcon />
-                <span>My Bookings</span>
-              </Link>
-            )}
-          </Menu.Item>
-          <div className="px-4 py-2">
-            <hr className="h-[1px] bg-gray-100" />
-          </div>
-          <h5 className="px-4 py-2 font-bold">My Cars</h5>
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/cars">
-                <CarsIcon />
-                <span>See My Cars</span>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/manage-bookings">
-                <ListIcon />
-                <span>My Car&apos;s Bookings</span>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/new-car">
-                <CarPlusIcon />
-                <span>Add New Car</span>
-              </Link>
-            )}
-          </Menu.Item>
-          <div className="px-4 py-2">
-            <hr className="h-[1px] bg-gray-100" />
-          </div>
-          <Menu.Item>
-            {({ active }) => (
-              <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/logout">
-                <LogoutIcon />
-                <span>Log Out</span>
-              </Link>
-            )}
-          </Menu.Item>
-        </Menu.Items>
+        {({ open }) => (
+          <>
+            <Menu.Button className="text-gray-100">{open ? 'Close' : 'Menu'}</Menu.Button>
+            <Menu.Items className="absolute top-24 flex flex-col rounded-lg border bg-indigo-400 text-gray-100">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/book">
+                    <CarIcon />
+                    <span>Book A Car</span>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/bookings">
+                    <PlateIcon />
+                    <span>My Bookings</span>
+                  </Link>
+                )}
+              </Menu.Item>
+              <div className="px-4 py-2">
+                <hr className="h-[1px] bg-gray-100" />
+              </div>
+              <h5 className="px-4 py-2 font-bold">My Cars</h5>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/cars">
+                    <CarsIcon />
+                    <span>See My Cars</span>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/manage-bookings">
+                    <ListIcon />
+                    <span>My Car&apos;s Bookings</span>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/new-car">
+                    <CarPlusIcon />
+                    <span>Add New Car</span>
+                  </Link>
+                )}
+              </Menu.Item>
+              <div className="px-4 py-2">
+                <hr className="h-[1px] bg-gray-100" />
+              </div>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link className={`nav-link ${active && 'bg-gray-800'}`} to="/logout">
+                    <LogoutIcon />
+                    <span>Log Out</span>
+                  </Link>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </>
+        )}
       </Menu>
       <Link to="/" className="absolute left-1/2 -translate-x-1/2 translate-y-2">
         <Logo className="w-18" />
