5160794b6f12943a8f209e33b9c671f4660b55c8
David Gusenga <89668775+gsngdavid@users.noreply.github.com>
2023-10-18 15:02:23 +0200

    Ft/font color setup
    * feat: add fonts and its configurations
    
    * feat: add custom colors
    
    * fix: applying requested changes
    
    * feat: add missing colors(indigo and gray)
    
    ---------
    
    Co-authored-by: Charlotte Divine <divinemaina@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //mustard
+        mustard: {
+          800: '#90A905',
+          400: '#BAC320',
+          200: '#EBF541',
+          100: '#F8FCAD',
+        },
+        lachs: {
+          800: '#EC903A',
+          400: '#F6A356',
+          200: '#FFC28A',
+          100: '#FFE3CA',
+        },
+        gray: {
+          800: '#111827',
+          400: '#6B7280',
+          200: '#C9CDD3',
+          100: '#F9FAFB',
+        },
+        indigo: {
+          800: '#265E78',
+          400: '#3E7591',
+          200: '#64A1C0',
+          100: '#DFEAF0',
+        },
+      },
+      fontFamily: {
+        inter: ['Inter'],
+        lora: ['Lora'],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
