83a38d26b10f0e96e4840396c9808ee85df53124
kareraolivier <kareraolivier@gmail.com>
2023-10-18 20:16:40 +0200

    fix: resolving conflicts

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { ReactElement } from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from '../NavBar'
 
 export default function Layout(): ReactElement {
   return (
     <div>
-      <p> Navigation Bar</p>
+      <NavBar />
       <Outlet />
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,11 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
