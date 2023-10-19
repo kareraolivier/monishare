785397aab568fd8247f99895effcb2ba6a816e2e
kareraolivier <kareraolivier@gmail.com>
2023-10-19 21:25:38 +0200

    feat: get single car

--- src/pages/CarDetailsPage.tsx
+++ src/pages/CarDetailsPage.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from 'react'
-import axios from 'axios'
-import { CarDto } from '../util/api'
+// import { CarDto } from '../util/api'
 import CarDetail from '../components/cars/CarDetail'
-import { useCars } from '../hooks'
+// import { useCars, useCarTypes } from '../hooks'
 
 export default function CarDetailsPage(): ReactElement {
-
+  // const [data] = useCars()
+  // const [type] = useCarTypes()
+  // console.log(data.data)
+  // console.log(type.data)
   return (
     <div>
       <CarDetail />
