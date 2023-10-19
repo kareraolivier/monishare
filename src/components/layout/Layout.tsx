7634a212ceccae8e5b2ae826d331ea76b3eea4d7
gsngdavid <gsngdavid@gmail.com>
2023-10-19 19:13:35 +0200

    feat: add color to the back arrow

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../NavBar'
+import Header from '../Header'
 
 export default function Layout(): ReactElement {
   return (
     <div>
       <NavBar />
+      <Header title="Available cars" />
       <Outlet />
     </div>
   )
