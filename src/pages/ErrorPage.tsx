6ce8d2b58acb6c455a6122b70a64831e027170e1
Charlotte Divine <divinemaina@gmail.com>
2023-10-19 19:09:46 +0200

    feat: add home and error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 import ErrorIcon from '../assets/ErrorIcon'
 export default function ErrorPage(): ReactElement {
   return (
-    <main className="mx-auto flex h-[95vh] flex-col items-center justify-center gap-8 py-10 lg:w-1/3">
+    <main className="mx-auto flex h-screen flex-col items-center justify-center gap-8 py-10 lg:w-1/3">
       <h1 className="flex flex-col items-center font-lora text-5xl font-extrabold text-gray-100">
         Oooops!
       </h1>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 
 export default function HomePage(): ReactElement {
   return (
-    <main className="mx-auto flex h-[95vh] flex-col justify-center gap-8 py-10 lg:w-1/3">
+    <main className="mx-auto flex h-screen flex-col justify-center gap-8 py-10 lg:w-1/3">
       <h1 className="flex flex-col items-center font-lora text-5xl font-extrabold text-gray-100">
         Moni <span className="font-weight-500 font-italic font-bold">share</span>
       </h1>
