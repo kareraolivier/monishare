bbb9abd4c44a04f43666418c2c833c39a06ff0aa
kareraolivier <kareraolivier@gmail.com>
2023-10-23 20:53:41 +0200

    feat: home page and error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -3,18 +3,16 @@ import ErrorIcon from '../assets/ErrorIcon'
 import Button from '../components/Button'
 export default function ErrorPage(): ReactElement {
   return (
-    <main className="mx-auto flex h-screen flex-col justify-center gap-8 py-10 lg:w-1/3">
-      <h1 className="flex flex-col items-center font-lora text-4xl font-extrabold text-gray-100">
-        OOOOOPS!
-      </h1>
+    <main className="mx-auto flex h-screen flex-col justify-center gap-8 py-10 font-lora md:w-1/2">
+      <h1 className="flex flex-col items-center text-4xl font-extrabold text-gray-100">OOOOOPS!</h1>
       <div className="mx-auto">
         <ErrorIcon />
       </div>
-      <p className="flex flex-col items-center font-lora text-lg text-gray-100">
+      <p className="flex flex-col items-center text-lg text-gray-100">
         Something went wrong.
-        <span className="font-weight-500 font-lora">We will solve your issue soon.</span>
+        <span className="font-weight-500">We will solve your issue soon.</span>
       </p>
-      <div className="flex flex-col gap-4 px-2 md:mx-auto md:w-2/3">
+      <div className="flex justify-center px-2 font-inter">
         <Button>Go back</Button>
       </div>
     </main>
