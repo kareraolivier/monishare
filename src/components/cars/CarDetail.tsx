896a5af47c6c2efecc84726fc56d234127f873b9
kareraolivier <kareraolivier@gmail.com>
2023-10-24 08:54:42 +0200

    feat: car details

--- src/components/cars/CarDetail.tsx
+++ src/components/cars/CarDetail.tsx
@@ -5,42 +5,49 @@ import PlateIcon from '../../assets/PlateIcon'
 import HorseIcon from '../../assets/HorseIcon'
 import FuelIcon from '../../assets/FuelIcon'
 import AlertIcon from '../../assets/AlertIcon'
-import { UserDto, CarTypeDto, CarDto } from '../../util/api'
-export default function CarDetail({
-  item,
-  carImage,
-  owner,
-}: {
-  item?: CarDto
-  carImage?: CarTypeDto
-  owner?: UserDto
-}): ReactElement {
+import { CarState, CarTypeDto, FuelType, UserDto } from '../../util/api'
+
+interface Checker {
+  licensePlate?: null
+  name: string
+  owner: UserDto
+  horsepower?: string
+  info?: string
+  plate?: string
+  fuelType: FuelType
+  carState: CarState
+}
+interface Pro {
+  item: Checker
+  carImage: CarTypeDto
+}
+export default function CarDetail(props: Pro): ReactElement {
   return (
-    <div className=" text-gray-100 flex flex-col md:flex-row justify-start md:items-center mx-w-[768px]">
-      <img src={carImage?.imageUrl} alt="carImage" className="md:w-1/2" />
+    <div className=" mx-w-[768px] flex flex-col justify-start text-gray-100 md:flex-row md:items-center">
+      <img src={props.carImage?.imageUrl} alt="carImage" className="md:w-1/2" />
       <div className="flex flex-col gap-2 p-8">
-        <h3 className="py-5 font-lora text-xl font-medium">{owner?.name}</h3>
+        <h3 className="py-5 font-lora text-xl font-medium">{props.item?.owner.name}</h3>
         <div className="flex items-center gap-2">
           <ProfileIcon />
-          <p>{carImage?.name}</p>
+          <p>{props.carImage?.name}</p>
         </div>
         <div className="flex items-center gap-2">
           <CarIcon />
-          <p>{item?.name}</p>
+          <p>{props.item?.name}</p>
         </div>
-        {item?.licensePlate !== null && (
+        {props.item?.licensePlate !== null && (
           <div className="flex items-center gap-2">
             <PlateIcon />
-            <p>{item?.licensePlate}</p>
+            <p>{props.item?.licensePlate}</p>
           </div>
         )}
         <div className="flex items-center gap-2">
           <HorseIcon />
-          <p>{item?.horsepower}hp</p>
+          <p>{props.item?.horsepower}hp</p>
         </div>
         <div className="flex items-center gap-2">
           <FuelIcon />
-          <p>{item?.fuelType}</p>
+          <p>{props.item?.fuelType}</p>
         </div>
         <div className="flex items-center gap-2">
           <AlertIcon />
--- src/pages/CarDetailsPage.tsx
+++ src/pages/CarDetailsPage.tsx
@@ -1,42 +1,39 @@
-// import { ReactElement, Suspense, useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import { CarDto, UserDto, CarTypeDto } from '../util/api'
-// import CarDetail from '../components/cars/CarDetail'
-// import { useCars, useCarTypes, useUser } from '../hooks'
-// import Header from '../components/Header'
-// export default function CarDetailsPage(): ReactElement {
-//   const param = useParams()
-//   const [cars, setCars] = useState<CarDto>()
-//   const [users, setUsers] = useState<UserDto>()
-//   const [images, setImages] = useState<CarTypeDto>()
-//   const fetchData = async () => {
-//     const [{ data, loading, error }] = await useCars()
-//     if (loading) {
-//       return <h1>Loading...</h1>
-//     }
+import { ReactElement } from 'react'
+import { useParams } from 'react-router-dom'
+import CarDetail from '../components/cars/CarDetail'
+import { useCars, useCarTypes, useUser } from '../hooks'
+import Header from '../components/Header'
+import { CarTypeDto, UserDto, FuelType, CarState } from '../util/api'
 
-//     if (!data) return <h1>No cars found</h1>
-//     const [{ data: image, loading: imageLoading }] = await useCarTypes()
-//     const item: CarDto = data?.filter(car => car.id === Number(param.id))[0]
-//     setCars(item)
-//     const carImage = image?.filter(el => el.id === item?.carTypeId)[0]
-//     if (carImage) {
-//       setImages(carImage)
-//     }
-//     const itemId: string | number = item?.ownerId
-//     const [{ data: owner, loading: ownerLoading, error: ownerError }] = await useUser(itemId)
-//     owner && setUsers(owner[0])
-//   }
-//   useEffect(() => {
-//     fetchData()
-//   }, [])
-//   console.log(cars, users, images)
-//   return (
-//     <div className="bg-indigo-800 px-4">
-//       <Header title="Available cars" />
-//       <div className=" flex flex-col justify-center items-center ">
-//         <CarDetail item={cars} owner={users} carImage={images} />
-//       </div>
-//     </div>
-//   )
-// }
+interface Checker {
+  licensePlate?: null
+  name: string
+  owner: UserDto
+  horsepower?: string
+  info?: string
+  plate?: string
+  fuelType: FuelType
+  carState: CarState
+}
+
+export default function CarDetailsPage(): ReactElement {
+  const param = useParams()
+
+  const [{ data }] = useCars()
+  const [{ data: image }] = useCarTypes()
+  const item = data?.filter(car => car.id === Number(param.id))[0]
+  const carImage = image?.filter(el => el.id === item?.carTypeId)[0] as CarTypeDto
+  const [{ data: owner, loading: ownerLoading }] = useUser(Number(item?.ownerId))
+  if (!owner || ownerLoading) return <p>Loading...</p>
+  const updatedData = { ...item, owner } as unknown as Checker
+  if (!updatedData) return <p>no data</p>
+
+  return (
+    <div className="bg-indigo-800 px-4">
+      <Header title="Available cars" />
+      <div className=" flex flex-col items-center justify-center ">
+        <CarDetail item={updatedData} carImage={carImage} />
+      </div>
+    </div>
+  )
+}
