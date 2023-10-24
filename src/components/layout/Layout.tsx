4d30d434e4edf18dba13049eb4f33757c4ac3e4e
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-24 17:37:43 +0200

    feat: add background color

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import NavBar from '../NavBar'
 
 export default function Layout(): ReactElement {
   return (
-    <div>
+    <div className="bg-indigo-800">
       <NavBar />
       <Outlet />
     </div>
