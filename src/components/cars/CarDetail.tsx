9504bc4577cc82a96f7fc8ce1c8b69a13ffa0825
kareraolivier <kareraolivier@gmail.com>
2023-10-25 22:54:01 +0200

    feat: updating car details

--- src/components/cars/CarDetail.tsx
+++ src/components/cars/CarDetail.tsx
@@ -5,21 +5,22 @@ import PlateIcon from '../../assets/PlateIcon'
 import HorseIcon from '../../assets/HorseIcon'
 import FuelIcon from '../../assets/FuelIcon'
 import AlertIcon from '../../assets/AlertIcon'
-import { CarTypeDto } from '../../util/api'
-import { SingleCarDetails } from '../../types/interface'
+import { CarTypeDto, CarDto, UserDto } from '../../util/api'
 
 export default function CarDetail({
   carData,
   carImage,
+  carOwner,
 }: {
-  carData: SingleCarDetails
+  carData: CarDto
   carImage?: CarTypeDto
+  carOwner?: UserDto
 }): ReactElement {
   return (
     <div className="mx-w-[768px] flex flex-col justify-start text-gray-100 md:flex-row md:items-center">
       <img src={carImage?.imageUrl} alt={carImage?.name} className="md:w-1/2" />
       <div className="flex flex-col gap-2 p-8">
-        <h3 className="py-5 font-lora text-xl font-medium">{carData?.owner?.name}</h3>
+        <h3 className="py-5 font-lora text-xl font-medium">{carOwner?.name}</h3>
         <div className="flex items-center gap-2">
           <ProfileIcon />
           <p>{carImage?.name}</p>
--- src/pages/CarDetailsPage.tsx
+++ src/pages/CarDetailsPage.tsx
@@ -3,25 +3,24 @@ import { useParams } from 'react-router-dom'
 import CarDetail from '../components/cars/CarDetail'
 import { useCars, useCarTypes, useUser } from '../hooks'
 import Header from '../components/Header'
-import { SingleCarDetails } from '../types/interface'
+import { CarDto } from '../util/api'
 
 export default function CarDetailsPage(): ReactElement {
   const { id } = useParams()
 
   const [{ data, loading }] = useCars()
   const [{ data: carTypes }] = useCarTypes()
-  const carData = data?.find(car => car.id === Number(id))
+  const carData = data?.find(car => car.id === Number(id)) as CarDto
   if (!loading && !carData) throw new Error('No cars found on that Id')
   const carImage = carTypes?.find(el => el.id === carData?.carTypeId)
-  const [{ data: owner, loading: ownerLoading }] = useUser(Number(carData?.ownerId))
-  ownerLoading ? <p>Loading...</p> : owner
-  const updatedData = { ...carData, owner: owner } as SingleCarDetails
+  const [{ data: carOwner, loading: carOwnerLoading }] = useUser(Number(carData?.ownerId))
+  carOwnerLoading ? <p>Loading...</p> : carOwner
 
   return (
     <div className="h-screen px-4 pt-16">
       <Header title="Details" />
       <div className=" flex flex-col items-center justify-center ">
-        <CarDetail carData={updatedData} carImage={carImage} />
+        {!loading && <CarDetail carData={carData} carImage={carImage} carOwner={carOwner} />}
       </div>
     </div>
   )
