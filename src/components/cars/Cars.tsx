c1d109902260c3bf9cee974d9859dbedcb6594c7
kareraolivier <kareraolivier@gmail.com>
2023-10-26 17:52:54 +0200

    fix: rename cars directory

---
+++ src/components/cars/Cars.tsx
@@ -0,0 +1,12 @@
+import Car from './CarCard'
+import { CarDetails } from '../../types/interfaces'
+
+export default function Cars({ cars }: { cars: CarDetails[] }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 lg:grid-cols-3">
+      {cars.map(car => (
+        <Car key={car.id} carDetails={car} />
+      ))}
+    </div>
+  )
+}
