c3ab815b19beb5a9793fdf97e1512b4866813d44
gsngdavid <gsngdavid@gmail.com>
2023-10-30 15:27:35 +0200

    feat: loop through menu items and remove limited width from navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,11 +5,9 @@ import NavBar from '../NavBar'
 export default function Layout(): ReactElement {
   return (
     <main className="min-h-screen bg-indigo-800">
-      <div className="mx-auto w-full max-w-[1500px]">
-        <NavBar />
-        <div className="px-4 pb-10 pt-20">
-          <Outlet />
-        </div>
+      <NavBar />
+      <div className="mx-auto w-full max-w-[1500px] px-4 pb-10 pt-20">
+        <Outlet />
       </div>
     </main>
   )
---
+++ src/data/navbar.js
@@ -0,0 +1,18 @@
+import CarIcon from '../assets/CarIcon'
+import BookingIcon from '../assets/BookingIcon'
+import CarsIcon from '../assets/CarsIcon'
+import ListIcon from '../assets/ListIcon'
+import CarPlusIcon from '../assets/CarPlusIcon'
+import LogoutIcon from '../assets/LogoutIcon'
+
+export const menuLink = [
+  { id: 1, link: '/book', text: 'Book A Car', icon: CarIcon },
+  { id: 2, link: '/bookings', text: 'My Bookings', icon: BookingIcon },
+  { id: 3, text: 'divider' },
+  { id: 4, title: 'My Cars' },
+  { id: 5, link: '/cars', text: 'See My Cars', icon: CarsIcon },
+  { id: 6, link: '/manage-bookings', text: 'My Car&apos;s Bookings', icon: ListIcon },
+  { id: 7, link: '/new-car', text: 'Add New Car', icon: CarPlusIcon },
+  { id: 8, text: 'divider' },
+  { id: 9, link: '/logout', text: 'Log Out', icon: LogoutIcon },
+]
