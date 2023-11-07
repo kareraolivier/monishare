36de7575eb007427f4f3b380e873e6193b4bfb0b
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-11-07 12:59:00 +0200

    feat: create home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,27 +3,21 @@ import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 export default function HomePage(): ReactElement {
   return (
-    <div className="mx-auto flex flex-col gap-8 pt-10 font-lora lg:w-1/3">
+    <main className="mx-auto flex h-screen flex-col  gap-8 py-10 font-lora lg:w-1/3">
       <h1 className="flex flex-col items-center text-5xl font-extrabold text-gray-100">
-        Moni <span className="font-italic font-medium">share</span>
+        MONI<span className="font-italic font-medium">share</span>
       </h1>
-      <p className="flex flex-col items-center text-lg text-gray-100">
-        Hello Manuela!
-        <span>What are you up to today?</span>
-      </p>
-      <div className="flex flex-col items-center gap-4 px-2 font-inter md:mx-auto md:w-2/3">
-        <Link to="/book" className="flex w-full justify-center">
-          <Button>Book a car</Button>
-        </Link>
-        <p className="text-center text-lg text-gray-100">or</p>
-        <Link to="/cars" className="flex w-full justify-center">
-          <Button filled={false}>See My Cars</Button>
-        </Link>
-
-        <Link to="/bookings" className="flex w-full justify-center">
-          <Button filled={false}>See My Bookings</Button>
-        </Link>
+      <div className="flex h-3/5 flex-col justify-around">
+        <p className="flex flex-col items-center text-lg text-gray-100">
+          Start sharing your Monis
+          <span>With the world</span>
+        </p>
+        <div className="flex flex-col items-center gap-4 px-2 font-inter md:mx-auto md:w-2/3">
+          <Link to="login" className="flex w-full justify-center">
+            <Button>Login</Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
