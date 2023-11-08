23672bb49e06983a04d3aa3f5e111f6c233eb9b5
charlottedivine <divinemaina@gmail.com>
2023-11-08 17:35:36 +0200

    feat: implement requested change

--- src/components/ui/inputs/SelectInput.tsx
+++ src/components/ui/inputs/SelectInput.tsx
@@ -1,9 +1,21 @@
-function SelectInput({ options }: { options: { id: number; text: string }[] }) {
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  options: {
+    id: number
+    text: string
+  }[]
+}
+
+function SelectInput({ options, name, value, onChange }: SelectProps) {
   return (
-    <div className=" max-w-sm rounded-[3rem] bg-indigo-200 p-3 shadow-xl">
-      <select name="select" className=" w-full bg-indigo-200 text-white outline-none">
+    <div className="max-w-sm rounded-[3rem] bg-indigo-200 p-3 shadow-xl">
+      <select
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="w-full bg-indigo-200 text-white outline-none"
+      >
         {options.map(option => (
-          <option key={option.id} className="p-3" value={option.text}>
+          <option key={option.id} value={option.text}>
             {option.text}
           </option>
         ))}
