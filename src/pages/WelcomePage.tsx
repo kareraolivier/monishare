5edcdc625b6e871fbdbdd4a548ec7d2ef5fb670c
kareraolivier <kareraolivier@gmail.com>
2023-11-14 20:23:10 +0200

    fix: add logged in user on welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,14 +1,21 @@
 import { ReactElement } from 'react'
 import Button from '../components/ui/Button'
-import { Link } from 'react-router-dom'
+import { useUser } from '../hooks'
+import { Link, Navigate } from 'react-router-dom'
 import Logo from '../components/Logo'
 
 export default function WelcomePage(): ReactElement {
+  const loggedInUserId = localStorage.getItem('userId')
+  if (loggedInUserId === null) return <Navigate to="/login" />
+
+  const [{ data: user, loading: userLoading, error: userError }] = useUser(loggedInUserId)
+  if (userError) throw new Error('No user found')
+
   return (
     <main className="mx-auto flex h-screen flex-col justify-center gap-8 py-10 font-lora lg:w-1/3">
       <Logo />
       <p className="flex flex-col items-center text-lg text-gray-100">
-        Hello Manuela!
+        {!userLoading && `Hello ${user?.name}!`}
         <span>What are you up to today?</span>
       </p>
       <div className="flex flex-col items-center gap-4 px-2 font-inter md:mx-auto md:w-2/3">
