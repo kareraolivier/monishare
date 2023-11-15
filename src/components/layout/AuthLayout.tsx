f35e3370ecad644158b91a992f44fd5258dfa8ae
gsngdavid <gsngdavid@gmail.com>
2023-11-15 14:25:01 +0200

    fix: put token inside the component

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
-const token = localStorage.getItem('token')
 export default function AuthLayout(): ReactElement {
+  const token = localStorage.getItem('token')
   return <>{token ? <Outlet /> : <Navigate to="/login" />}</>
 }
