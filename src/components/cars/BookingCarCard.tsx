ebb876ae21c417f997016dcc67281be8265e38da
gsngdavid <gsngdavid@gmail.com>
2023-11-21 19:11:53 +0200

    feat: get dynamic dat through props

--- src/components/cars/BookingCarCard.tsx
+++ src/components/cars/BookingCarCard.tsx
@@ -1,48 +1,69 @@
 import { ReactElement } from 'react'
-import { BookingDto } from '../../util/api'
 import CalendarIcon from '../../assets/CalendarIcon'
 import TimeIcon from '../../assets/TimeIcon'
+import { Action } from '../../types/enums'
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 interface Props {
-  carDetails: BookingDto
-  action: string
+  carDetails: {
+    id: number
+    name: string
+    image: string
+    action: Action
+    user: string
+    startDate: Date
+    endDate: Date
+  }
 }
 
-export default function BookingCarCard({ carDetails, action }: Props): ReactElement {
-  console.log(carDetails)
+export default function BookingCarCard({ carDetails }: Props): ReactElement {
+  const startDate = `${carDetails.startDate.getDate()} ${
+    months[carDetails.startDate.getMonth()]
+  } ${carDetails.startDate.getFullYear()}`
+  const startMinutes = carDetails.startDate.getMinutes()
+  const startTime = `${carDetails.startDate.getHours()}:${
+    Number(startMinutes) >= 10 ? startMinutes : '0' + startMinutes
+  }`
+
+  const endDate = `${carDetails.endDate.getDate()} ${months[carDetails.endDate.getMonth()]}
+  ${carDetails.endDate.getFullYear()}`
+  const endMinutes = carDetails.endDate.getMinutes()
+  const endTime = `${carDetails.endDate.getHours()}:${
+    Number(endMinutes) >= 10 ? endMinutes : '0' + endMinutes
+  }`
+
   return (
     <div className="flex max-w-xl flex-col justify-start overflow-x-hidden text-gray-100 md:max-w-none md:flex-row md:items-center">
-      <img
-        src="https://s3.eu-central-1.amazonaws.com/assets.monishare.ojemba/cooper.png"
-        alt="Booking car"
-        className="animate-slideLeft md:w-1/2"
-      />
+      <img src={carDetails.image} alt={carDetails.name} className="animate-slideLeft md:w-1/2" />
       <div className="flex animate-slideRight flex-col gap-2 p-8">
         <div className="mb-5 space-y-2">
-          <h3 className="font-lora text-2xl font-medium">Mighty Mouse</h3>
-          <h5 className="font-inter font-normal">{action} by Manuela</h5>
+          <h3 className="font-lora text-2xl font-medium">{carDetails.name}</h3>
+          <h5 className="font-inter font-normal">
+            {carDetails.action} by {carDetails.user}
+          </h5>
         </div>
         <div className="flex flex-col justify-between gap-5 min-[325px]:flex-row">
           <div className="space-y-3">
             <h1>from</h1>
             <div className="flex items-center gap-2">
               <CalendarIcon />
-              <p>14 Jun 2023</p>
+              <p>{startDate}</p>
             </div>
             <div className="flex items-center gap-2">
               <TimeIcon />
-              <p>15:00</p>
+              <p>{startTime}</p>
             </div>
           </div>
           <div className="space-y-3">
             <h1>to</h1>
             <div className="flex items-center gap-2">
               <CalendarIcon />
-              <p>14 Jun 2023</p>
+              <p>{endDate}</p>
             </div>
             <div className="flex items-center gap-2">
               <TimeIcon />
-              <p>15:00</p>
+              <p>{endTime}</p>
             </div>
           </div>
         </div>
