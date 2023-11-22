7afc966a715419f0e144dc4604c64832bdd33559
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-11-22 12:37:12 +0200

    feat: implement the desktop version of start and end date

--- src/components/cars/DateRangePicker.tsx
+++ src/components/cars/DateRangePicker.tsx
@@ -7,56 +7,57 @@ import { ReactElement } from 'react'
 import Button from '../ui/Button'
 import { ButtonVariant } from '../../types/enums'
 import Header from '../ui/Header'
-import Cars from './Cars'
 
 const title = 'BOOK CAR'
 
 export default function DateRangePicker(): ReactElement {
   return (
-    <div className="w-full max-w-sm space-y-12 font-inter text-gray-100">
+    <>
       <Header title={title} />
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer
-          components={[
-            'DateTimePicker',
-            'MobileDateTimePicker',
-            'DesktopDateTimePicker',
-            'StaticDateTimePicker',
-          ]}
-        >
-          <DemoItem label="Start date">
-            <MobileDateTimePicker
-              defaultValue={dayjs('2022-04-17T15:30')}
-              className="rounded-full  bg-indigo-200 text-white"
-              sx={{
-                '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-                  border: 'none',
-                },
-                '& .MuiOutlinedInput-root .MuiInputBase-input': {
-                  color: 'white',
-                },
-              }}
-            />
-          </DemoItem>
-          <DemoItem label="End date">
-            <MobileDateTimePicker
-              defaultValue={dayjs('2022-04-17T15:30')}
-              className="rounded-full bg-indigo-200"
-              sx={{
-                '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-                  border: 'none',
-                },
-                '& .MuiOutlinedInput-root .MuiInputBase-input': {
-                  color: 'white',
-                },
-              }}
-            />
-          </DemoItem>
-        </DemoContainer>
-      </LocalizationProvider>
-      <Button filled={true} variant={ButtonVariant.Primary}>
-        Search Available Cars
-      </Button>
-    </div>
+      <div className="mx-auto max-w-sm space-y-12 font-inter text-gray-100 ">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer
+            components={[
+              'DateTimePicker',
+              'MobileDateTimePicker',
+              'DesktopDateTimePicker',
+              'StaticDateTimePicker',
+            ]}
+          >
+            <DemoItem label="Start date">
+              <MobileDateTimePicker
+                defaultValue={dayjs('2022-04-17T15:30')}
+                className="rounded-full  bg-indigo-200 text-white"
+                sx={{
+                  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                    border: 'none',
+                  },
+                  '& .MuiOutlinedInput-root .MuiInputBase-input': {
+                    color: 'white',
+                  },
+                }}
+              />
+            </DemoItem>
+            <DemoItem label="End date">
+              <MobileDateTimePicker
+                defaultValue={dayjs('2022-04-17T15:30')}
+                className=" rounded-full bg-indigo-200"
+                sx={{
+                  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                    border: 'none',
+                  },
+                  '& .MuiOutlinedInput-root .MuiInputBase-input': {
+                    color: 'white',
+                  },
+                }}
+              />
+            </DemoItem>
+          </DemoContainer>
+        </LocalizationProvider>
+        <Button filled={true} variant={ButtonVariant.Primary}>
+          Search Available Cars
+        </Button>
+      </div>
+    </>
   )
 }
