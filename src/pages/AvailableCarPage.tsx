f95954d34bdc9df4757f2b8ccc95949db7178e5e
gsngdavid <gsngdavid@gmail.com>
2023-11-28 18:05:15 +0200

    feat: display only available cars

--- src/pages/AvailableCarPage.tsx
+++ src/pages/AvailableCarPage.tsx
@@ -6,33 +6,27 @@ import Loading, { LoadingStyle } from '../components/ui/Loading'
 import { useSearchParams, Navigate } from 'react-router-dom'
 import { useReadLocalStorage } from 'usehooks-ts'
 import { BookCar } from '../types/interfaces'
-import { BookingDto, CarDto } from '../util/api'
-import { isValidRange } from '../helpers/frontend'
+import dayjs from 'dayjs'
+import { getAvailableCars } from '../helpers/frontend'
 
 const title = 'Available Cars'
-
-interface CombinedCars {
-  [key: number]: { car: CarDto; bookings: BookingDto[] }
-}
 
 export default function AvailableCarPage(): ReactElement {
   const loggedInUserId = useReadLocalStorage('userId')
   if (loggedInUserId === null) return <Navigate to="/login" />
+
   const [searchParams] = useSearchParams()
   const [{ data: cars, loading: carsLoading, error: carsError }] = useCars()
   const [{ data: users, loading: usersLoading, error: usersError }] = useUsers()
   const [{ data: carTypes, loading: carTypesLoading, error: carTypesError }] = useCarTypes()
+  const { data: bookings, loading: bookingsLoading, error: bookingsError } = useBookings()
   const [{ data: bookedCar, loading: bookedCarLoading, error: bookedCarError }, executeBookCar] =
     useBookCar()
-  const { data: bookings, loading: bookingsLoading, error: bookingsError } = useBookings()
 
-  const startDate = searchParams.get('startDate')
-  const endDate = searchParams.get('endDate')
+  const startDate = dayjs(searchParams.get('startDate'))
+  const endDate = dayjs(searchParams.get('endDate'))
 
-  if (!startDate || !endDate) throw new Error('Please provide valid dates')
-
-  if (bookedCarError) throw new Error('Booking car was not successful, sorry for inconvenience🙏')
-  if (!bookedCarLoading && bookedCar) return <Navigate to="/bookings" />
+  if (!startDate.isValid() || !endDate.isValid()) throw new Error('Please provide valid dates')
 
   if (carsError || usersError || carTypesError || bookingsError)
     throw new Error('Fetching cars was not successful, sorry for inconvenience🙏')
@@ -45,39 +39,24 @@ export default function AvailableCarPage(): ReactElement {
       </>
     )
 
-  const notUserCars = cars?.filter(car => car.ownerId !== Number(loggedInUserId))
+  if (bookedCarError) throw new Error('Booking car was not successful, sorry for inconvenience🙏')
+  if (!bookedCarLoading && bookedCar) return <Navigate to="/bookings" />
 
-  const bookedCars = notUserCars?.filter(car => bookings?.some(booking => booking.carId === car.id))
+  const availableCars = getAvailableCars({
+    loggedInUserId: Number(loggedInUserId),
+    cars,
+    bookings,
+    bookingRange: { startDate, endDate },
+  })
 
-  const bookedCarsBookings = bookedCars?.reduce((combinedCars: CombinedCars, bookedCar: CarDto) => {
-    const bookedCarBookings: BookingDto[] = []
-    bookings?.forEach(booking => {
-      if (booking.carId !== bookedCar.id) return
-      bookedCarBookings.push(booking)
-    })
-    combinedCars[bookedCar.id] = { car: bookedCar, bookings: bookedCarBookings }
-    return combinedCars
-  }, {})
-
-  const availableBookedCarIds = Object.keys(bookedCarsBookings ?? []).filter(carId =>
-    (bookedCarsBookings ?? [])[Number(carId)].bookings.every(booking =>
-      isValidRange(
-        { startDate: new Date(startDate), endDate: new Date(endDate) },
-        { startDate: new Date(booking.startDate), endDate: new Date(booking.endDate) },
-      ),
-    ),
-  )
-  console.log(availableBookedCarIds.length, Object.keys(bookedCarsBookings ?? []))
-
-  if (notUserCars?.length === 0)
-    const currrentUserCars = cars?.filter(car => car.ownerId !== Number(loggedInUserId))
-  if (currrentUserCars?.length === 0)
+  if (availableCars.length === 0)
     return (
       <>
         <Header title={title} />
         <h1 className="text-center text-2xl text-white">No cars found for this time slot!</h1>
       </>
     )
+
   function onBookCar(carId?: number) {
     const startDate = searchParams.get('startDate')
     const endDate = searchParams.get('endDate')
@@ -90,12 +69,13 @@ export default function AvailableCarPage(): ReactElement {
       data: bookCar,
     })
   }
-  const populatedCars = currrentUserCars?.map(car => {
+
+  const populatedCars = availableCars.map(car => {
     const owner = users?.find(user => car.ownerId === user.id)
     const type = carTypes?.find(carType => car.carTypeId === carType.id)
     return {
-      id: car?.id,
-      name: car?.name,
+      id: car.id,
+      name: car.name,
       owner: owner?.name,
       ownerId: owner?.id,
       type: type?.name,
