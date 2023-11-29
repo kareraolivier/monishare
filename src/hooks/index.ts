e681ac16ba2ee9873fda8c3950d63ab5eeea6909
kareraolivier <kareraolivier@gmail.com>
2023-11-29 16:55:08 +0200

    feat: add new function to fetch bookings

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,19 @@
-import useBookings from './useBookings'
+import useMyBookings from './useMyBookings'
 import useCars from './useCars'
 import useCarTypes from './useCarTypes'
 import useUser from './useUser'
 import useUsers from './useUsers'
 import useAddCar from './useAddCar'
 import useBookCar from './useBookCar'
+import useManageBookings from './useManageBookings'
 
-export { useBookings, useCars, useCarTypes, useUser, useUsers, useAddCar, useBookCar }
+export {
+  useManageBookings,
+  useMyBookings,
+  useCars,
+  useCarTypes,
+  useUser,
+  useUsers,
+  useAddCar,
+  useBookCar,
+}
