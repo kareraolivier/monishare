40b72b2bab077b750ca9fdaabb0c7814f8aebb1e
gsngdavid <gsngdavid@gmail.com>
2023-11-30 13:25:12 +0200

    refactor: book car page

--- src/components/cars/DateRangePicker.tsx
+++ src/components/cars/DateRangePicker.tsx
@@ -1,20 +1,13 @@
 import dayjs, { Dayjs } from 'dayjs'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { LocalizationProvider, MobileDateTimePicker } from '@mui/x-date-pickers'
+import { LocalizationProvider } from '@mui/x-date-pickers'
 import { ReactElement, useState } from 'react'
 import Button from '../ui/Button'
 import Header from '../ui/Header'
 import { useNavigate } from 'react-router-dom'
+import DatePicker from '../ui/inputs/DatePicker'
 
 const title = 'BOOK CAR'
-const datePickerStyles = {
-  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-    border: 'none',
-  },
-  '& .MuiOutlinedInput-root .MuiInputBase-input': {
-    color: 'white',
-  },
-}
 
 export default function DateRangePicker(): ReactElement {
   const [startDate, setStartDate] = useState(dayjs())
@@ -22,43 +15,31 @@ export default function DateRangePicker(): ReactElement {
 
   const navigate = useNavigate()
 
-  const startDateChangeHandler = (newStartDate: Dayjs | null) => {
-    if (newStartDate) setStartDate(newStartDate)
-  }
-  const endDateChangeHandler = (newEndDate: Dayjs | null) => {
-    if (newEndDate) setEndDate(newEndDate)
-  }
+  const startDateChangeHandler = (date: Dayjs | null) => date && setStartDate(date)
+  const endDateChangeHandler = (date: Dayjs | null) => date && setEndDate(date)
 
   const searchClickHandler = () => {
-    const start = startDate.toISOString()
-    const end = endDate.toISOString()
-    navigate(`/available-cars?startDate=${start}&&endDate=${end}`)
+    navigate(
+      `/available-cars?startDate=${startDate.toISOString()}&&endDate=${endDate.toISOString()}`,
+    )
   }
   return (
     <>
       <Header title={title} />
       <div className="mx-auto max-w-sm space-y-12 font-inter text-gray-100 ">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <div className="grid gap-2">
-            <label>Start date</label>
-            <MobileDateTimePicker
-              value={startDate}
-              onChange={startDateChangeHandler}
-              minDateTime={dayjs()}
-              className="rounded-full bg-indigo-200 text-white"
-              sx={datePickerStyles}
-            />
-          </div>
-          <div className="grid gap-2">
-            <label>End date</label>
-            <MobileDateTimePicker
-              value={startDate.add(2, 'hours')}
-              onChange={endDateChangeHandler}
-              minDateTime={startDate}
-              className="rounded-full bg-indigo-200"
-              sx={datePickerStyles}
-            />
-          </div>
+          <DatePicker
+            value={startDate}
+            label="Start date"
+            onChange={startDateChangeHandler}
+            minDateTime={dayjs()}
+          />
+          <DatePicker
+            value={startDate.add(2, 'hours')}
+            label="End date"
+            onChange={endDateChangeHandler}
+            minDateTime={startDate}
+          />
         </LocalizationProvider>
         <Button filled={true} onClick={searchClickHandler}>
           Search Available Cars
